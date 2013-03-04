9f07d2eb0e79cd03c3c4415fe8face6c296c85ba
Greg Lu <greg.lu@where.com>
2013-03-04 09:13:44 -0500

    Subscribe by webpage

--- hub.js
+++ hub.js
@@ -3,6 +3,7 @@ var dnode = require('dnode');
 var querystring = require('querystring');
 var levelup = require('levelup');
 var url = require('url');
+var ecstatic = require('ecstatic')(__dirname + '/public');
 
 var subscribers = levelup('./subscribers');
 
@@ -11,13 +12,16 @@ var server_remote;
 
 var server = http.createServer(function (req, res) {
   if (req.method == "POST") {
-    var subscriber = "";
+    var bodyData = "";
 
     req.on('data', function(chunk) {
-      subscriber += chunk;
+      bodyData += chunk;
     });
 
     req.on("end", function() {
+      var parsedBodyData = querystring.parse(bodyData);
+      var subscriber = !!(parsedBodyData.subscriber) ? parsedBodyData.subscriber : bodyData;
+
       var subscriberUrl = url.parse(subscriber);
       if (subscriberUrl.protocol && subscriberUrl.host && subscriberUrl.path) {
         subscribers.put(subscriber, 1);
@@ -28,14 +32,34 @@ var server = http.createServer(function (req, res) {
         res.end('Invalid URL. Subscription not added.');
       }
     });
-  } else {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('eirobridge hub');
+  } else if (req.method == "DELETE") {
+    var bodyData = "";
 
+    req.on('data', function(chunk) {
+      bodyData += chunk;
+    });
+
+    req.on("end", function() {
+      var parsedBodyData = querystring.parse(bodyData);
+      var subscriber = !!(parsedBodyData.subscriber) ? parsedBodyData.subscriber : bodyData;
+
+      var subscriberUrl = url.parse(subscriber);
+      if (subscriberUrl.protocol && subscriberUrl.host && subscriberUrl.path) {
+        subscribers.del(subscriber);
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.end(subscriber + ' successfully deleted');
+      } else {
+        res.writeHead(406, {'Content-Type': 'text/plain'});
+        res.end('Invalid URL. Subscription not added.');
+      }
+    });
+  } else {
     subscribers.createKeyStream()
       .on('data', function (data) {
         console.log('key=', data)
       });
+
+    return ecstatic(req, res);
   }
 });
 
@@ -46,8 +70,8 @@ function connect() {
 
   var req = http.request({
     port: 8008,
-    // hostname: 'www.heuheuheu.com',
-    hostname: 'localhost',
+    hostname: 'www.heuheuheu.com',
+    // hostname: 'localhost',
     headers: {
       'Connection': 'Upgrade',
       'Upgrade': 'websocket'
