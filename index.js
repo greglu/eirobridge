da32096845722456411b0055908a42b1e514d8ad
Greg Lu <greg.lu@where.com>
2013-03-04 16:46:40 -0500

    VERY IMPORTANT UPDATE

--- index.js
+++ index.js
@@ -36,8 +36,8 @@ var server = http.createServer(function (req, res) {
       });
     });
   } else {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('eirobridge');
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end('<img src="http://i.imgur.com/uJMl9Lj.jpg" />');
   }
 });
 
